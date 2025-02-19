24fd2ef65a300b5f5c05eed7a8edfeb2ae560f35
yanis lamliji <yanis.lamliji@ynov.com>
2025-02-19 08:49:27 +0100

    article page addition

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,33 +10,17 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class ArticleController extends AbstractController
 {
-    #[Route('/article', name: 'article_view')]
-    public function show(EntityManagerInterface $entityManager): Response
+    #[Route('/article/{index}', name: 'article_view')]
+    public function show(EntityManagerInterface $entityManager, int $index): Response
     {
-        $articles = $entityManager->getRepository(Article::class)->findAll();
+        $article_page = array();
+        for($ind = 0; $ind < 9; $ind++)
+        {
+            array_push($article_page, $ind + (9 * $index));
+        }
+        $articles = $entityManager->getRepository(Article::class)->findBy(array('id' => $article_page));
         return $this->render('article/index.html.twig', [
             'articles' => $articles,
         ]);
-    }
-
-    /**
- * @Route("/article", name: "article_index")
- * @param Index $index
- * @param Articles $articles
- * @param Max $max
- */
-    function Index_Page(int $index,array $articles, int $max)
-    {
-        $article_page = array();
-        for($page = 0; $page < $max; $page++)
-        {
-            try{
-                array_push($article_page, $articles[$page * $index]);
-            }
-            catch (Exception){
-                return $article_page;
-            }
-        }
-        return $article_page;
     }
 }
