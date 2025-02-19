a8bb3c02b2f1f2ab0d0ef8277b51df01cbfa26ac
yanis lamliji <yanis.lamliji@ynov.com>
2025-02-19 08:23:48 +0100

    add main homepage

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,14 +10,33 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class ArticleController extends AbstractController
 {
-
     #[Route('/article', name: 'article_view')]
     public function show(EntityManagerInterface $entityManager): Response
     {
-        $id = 0;
         $articles = $entityManager->getRepository(Article::class)->findAll();
         return $this->render('article/index.html.twig', [
             'articles' => $articles,
         ]);
+    }
+
+    /**
+ * @Route("/article", name: "article_index")
+ * @param Index $index
+ * @param Articles $articles
+ * @param Max $max
+ */
+    function Index_Page(int $index,array $articles, int $max)
+    {
+        $article_page = array();
+        for($page = 0; $page < $max; $page++)
+        {
+            try{
+                array_push($article_page, $articles[$page * $index]);
+            }
+            catch (Exception){
+                return $article_page;
+            }
+        }
+        return $article_page;
     }
 }
