db7601ccaf1ac89f9b405cc4cf995da846583939
yanis lamliji <yanis.lamliji@ynov.com>
2025-02-20 12:01:32 +0100

    make upload image file

--- src/Controller/SellController.php
+++ src/Controller/SellController.php
@@ -9,6 +9,9 @@ use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Article;
 use \Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Uid\Uuid;
+use App\Form\ArticleType;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 final class SellController extends AbstractController{
     #[Route('/sell', name: 'SellPage',methods: 'GET')]
     public function index(): Response
@@ -18,9 +21,11 @@ final class SellController extends AbstractController{
         ]);
     }
     #[Route('/sell', name: 'CreateArticle',methods: 'POST')]
-    public function CreateArticle(Request $request,EntityManagerInterface $entityManager): Response
+    public function CreateArticle(Request $request,EntityManagerInterface $entityManager, string $imageDirectory): Response
     {
         $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
         $user = $this->getUser();
         $article->setCreatorID($user);
         $article->setName($request->request->get('name'));
@@ -28,11 +33,33 @@ final class SellController extends AbstractController{
         $article->setPrice($request->request->get('price'));
         $article->setPublicationDate(new \DateTime());
         $article->setUid(Uuid::v4()->toRfc4122());
+        
+        // if ($form->isSubmitted() && $form->isValid()) {
+        //     /** @var UploadedFile $imageFile */
+        //     $imageFile = $form->get('upload_image')->getData();
+
+        //     if ($imageFile) {
+        //         $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+
+        //         $safeFilename = $slugger->slug($originalFilename);
+        //         $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+        //         try {
+        //             $imageFile->move($imageDirectory, $newFilename);
+        //         } catch (FileException $e) {
+        //             // ... handle exception if something happens during file upload
+        //         }
+        //         $article->setImage($newFilename);
+        //     }
+        //     return $this->redirectToRoute('app_image_list');
+        // }
+        
+        // $entityManager->persist($article);
         $article->setImage("");
-        $entityManager->persist($article);
         $entityManager->flush();
         return $this->render('sell/index.html.twig', [
             'controller_name' => 'SellController',
+            'form' => $form,
         ]);
     }
 }
