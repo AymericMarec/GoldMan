bfcefdef6c4ee47e131f78f6ad656b14173395b1
Maymeric <aymeric.marec@gmail.com>
2025-02-21 15:15:02 +0100

    add feater to footer

--- public/js/search.js
+++ public/js/search.js
@@ -1,5 +1,5 @@
-let search = document.getElementById("search")
-
 function Search(){
+    var search = document.getElementById("search")
+    console.log(search)
     window.location.href = window.location.origin+"/article/1?search="+search.value;
 }
